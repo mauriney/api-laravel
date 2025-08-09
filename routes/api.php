59fdb07ff0b72ea020160ddfa27bb56dffa63e0c
Mauriney <maurineyy@gmail.com>
2025-08-09 15:50:00 -0300

    feat: padronizando response e create com validacoes

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,5 @@ Route::prefix('v1')->group(function() {
     Route::get('/users/{user}', [UserController::class, 'show']);
     Route::get('/invoices', [InvoiceController::class, 'index']);
     Route::get('/invoices/{invoice}', [InvoiceController::class, 'show']);
+    Route::post('/invoices', [InvoiceController::class, 'store']);
 });
